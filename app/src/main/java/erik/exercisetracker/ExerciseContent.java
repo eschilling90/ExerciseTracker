58869599bafa90fe05a8f28653a15c668cfcf077
Erik <erik.schilling@gmail.com>
2015-08-14 18:03:25 -0400

    added create exercise functionality

---
+++ app/src/main/java/erik/exercisetracker/ExerciseContent.java
@@ -0,0 +1,17 @@
+package erik.exercisetracker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by eriks_000 on 8/14/2015.
+ */
+public class ExerciseContent {
+    int exerciseId;
+    String name;
+    int rating;
+    String notes;
+    String multimedia;
+    String description;
+    String tags;
+}
